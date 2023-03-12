9941ea7825ee27f47fdcb043c1c98ea6b7c4d6d5
jelolu1 <jelolu1@gmail.com>
2023-03-12 16:56:15 +0000

    Modified grid style

--- src/components/Champion.tsx
+++ src/components/Champion.tsx
@@ -5,7 +5,7 @@ import style from './Champion.module.scss';
 export const Champion = ({ championInfo }: ChampionPropsType) => {
 	const navigate = useNavigate();
 
-	const championName = championInfo.id
+	const championName = championInfo.name
 		.split('')
 		.map((c, i) => (i === 0 ? c : c === c.toUpperCase() ? ' ' + c : c))
 		.join('');
@@ -22,6 +22,7 @@ export const Champion = ({ championInfo }: ChampionPropsType) => {
 				alt={championName}
 				onClick={handleClick}
 			/>
+
 			<h2 className={style['champion-title']} onClick={handleClick}>
 				{championName}
 			</h2>
